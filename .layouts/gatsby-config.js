78c951829143a2f029223a91e526adcace86a585
MarsAuthority <chinamarsmail@gmail.com>
2023-04-10 09:46:05 +0800

    1

--- .layouts/gatsby-config.js
+++ .layouts/gatsby-config.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   description: "自我提升,效率提升,开源工具,学习笔记,网络安全",
   twitterName: "theowenyoung",
   imageUrl: "/graph-visualisation.jpg",
-  siteUrl: "https://test.mars.run",
+  siteUrl: "https://mars.run",
 };
 module.exports = {
   siteMetadata,
